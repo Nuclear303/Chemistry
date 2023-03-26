a81cbb3765fa15b59963da1f90b310af91a873d4
Mikołaj Morzycki <mikolajmorzycki@MacBook-Air-Mikoaj.local>
2023-03-26 18:06:39 +0200

    naprawa menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,26 +1,37 @@
-
 $(document).ready(_=>{
-    $(".temat2").hover(
-        function(){
-            $(".temat2-menu").slideDown()
-        },
-        function(){
-            $(".temat2-menu").slideUp()
-        });
-    $(".temat3").hover(
-        function(){
-            $(".temat3-menu").slideDown()
-        },
-        function(){
-            $(".temat3-menu").slideUp()
-        });
-    $(".temat1").hover(
-        function(){
-            $(".temat1-menu").slideDown()
-        },
-        function(){
-            $(".temat1-menu").slideUp()
-        });
+    $(window).resize(function(){
+        let x = window.innerWidth;
+        document.querySelector(".content").textContent=x;
+        if(x>842){
+            $(".temat2").hover(
+                function(){
+                    $(".temat2-menu").slideDown()
+                },
+                function(){
+                    $(".temat2-menu").slideUp()
+                });
+            $(".temat3").hover(
+                function(){
+                    $(".temat3-menu").slideDown()
+                },
+                function(){
+                    $(".temat3-menu").slideUp()
+                });
+            $(".temat1").hover(
+                function(){
+                    $(".temat1-menu").slideDown()
+                },
+                function(){
+                    $(".temat1-menu").slideUp()
+                });
+        }
+        else{
+            $(".temat2").off("mouseenter mouseleave");
+            $(".temat3").off("mouseenter mouseleave");
+            $(".temat1").off("mouseenter mouseleave");
+        }
+    });
+    
     $(".home-li").hover(function(){
         $(this).animate({'letter-spacing':'4.5pt'},250);
     },
@@ -30,4 +41,3 @@ $(document).ready(_=>{
     );
 });
 
-let x = screen.width;
