cd27407699019382f1f2414cfe536b4bf1592590
Mikolaj Morzycki <m.morzycki@wp.pl>
2023-03-26 23:11:07 +0200

    poprawa menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,26 +1,36 @@
 $(document).ready(_=>{
     let x = window.innerWidth;
-    $(".temat2").hover(
-        function(){
-            $(".temat2-menu").slideDown()
-        },
-        function(){
-            $(".temat2-menu").slideUp()
-        });
-    $(".temat3").hover(
-        function(){
-            $(".temat3-menu").slideDown()
-        },
-        function(){
-            $(".temat3-menu").slideUp()
-        });
-    $(".temat1").hover(
-        function(){
-            $(".temat1-menu").slideDown()
-        },
-        function(){
-            $(".temat1-menu").slideUp()
-        });
+    if(x<842){
+        $(".temat2").off("mouseenter mouseleave");
+        $(".temat3").off("mouseenter mouseleave");
+        $(".temat1").off("mouseenter mouseleave");
+        $(".temat2").click();
+        $(".temat3").click();
+        $(".temat1").click();
+    }
+    else{
+        $(".temat2").hover(
+            function(){
+                $(".temat2-menu").slideDown()
+            },
+            function(){
+                $(".temat2-menu").slideUp()
+            });
+        $(".temat3").hover(
+            function(){
+                $(".temat3-menu").slideDown()
+            },
+            function(){
+                $(".temat3-menu").slideUp()
+            });
+        $(".temat1").hover(
+            function(){
+                $(".temat1-menu").slideDown()
+            },
+            function(){
+                $(".temat1-menu").slideUp()
+            });
+    }
     $(window).resize(function(){
         let x = window.innerWidth;
         if(x<842){
